1b918f6ca4cf569bfe59e65115bf83fd6add5fc9
jmatheney1 <35322081+jmatheney1@users.noreply.github.com>
2018-02-22 22:14:46 -0600

    lesson 7 updates

---
+++ assignments/lesson-7/scripts/currentdate.js
@@ -0,0 +1,30 @@
+    var d = new Date();/* no function needed*/
+    var weekday = new Array(7);
+        weekday[0] = "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+    var n = weekday[d.getDay()];
+
+    var month = new Array(12);
+        month[0] = "Jan";
+        month[1] = "Feb";
+        month[2] = "Mar";
+        month[3] = "Apr";
+        month[4] = "May";
+        month[5] = "Jun";
+        month[6] = "July";
+        month[7] = "Aug";
+        month[8] = "Sep";
+        month[9] = "Oct";
+        month[10] = "Nov";
+        month[11] = "Dec";
+    var m = month[d.getMonth()];
+
+    
+    document.getElementById("date").innerHTML = n + ", " + d.getDate() + " " + m + " " + d.getFullYear();
+
+/*Monday, 6 April 2020*/
