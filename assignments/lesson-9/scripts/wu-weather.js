48df39377bc665c2650229f839fbc376bd5c2d09
jmatheney1 <35322081+jmatheney1@users.noreply.github.com>
2018-03-08 00:49:23 -0600

    l9

---
+++ assignments/lesson-9/scripts/wu-weather.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+
+var weatherObject = new XMLHttpRequest();
+
+weatherObject.open('GET', 'http://api.wunderground.com/api/343d8105ba55767e/conditions/q/MN/Franklin.json', true  );
+
+weatherObject.send();
+
+weatherObject.onload = function () {
+    
+    var weatherInfo = JSON.parse(weatherObject.responseText);
+    console.log(weatherInfo);
+    
+    document.getElementById('place').innerHTML =  weatherInfo.current_observation.display_location.full;
+    document.getElementById('temp_f').innerHTML = weatherInfo.current_observation.temp_f;
+    document.getElementById('w_icon').src = weatherInfo.current_observation.icon_url;
+    document.getElementById('wind_mph').innerHTML = weatherInfo.current_observation.wind_mph;
+    document.getElementById('weather').innerHTML = weatherInfo.current_observation.weather;
+    
+    
+}; // end of onload --- document.getElementById('').innerHTML = weatherInfo;
